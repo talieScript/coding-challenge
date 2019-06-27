88f87f33dfc740312bb3db93c1c4512ecd13c9ac
Taliesin <taliesin.bowes@hotmail.co.uk>
2019-06-27 10:39:19 +0100

    Created test input and layed-out functions to me made

---
+++ index.js
@@ -0,0 +1,30 @@
+const testInput = {
+    map: [5, 3],
+    shipSequences: {
+        1: {
+            coordinates: [1, 1, "e"],
+            input: "rfrfrfrf"
+        },
+        2: {
+            coordinates: [3, 2, "n"],
+            input: "frrfllffrrfll"
+        },
+        3: {
+            coordinates: [0, 3, "w"],
+            input: "llffflflfl"
+        }
+    }
+}
+
+function makeMap(mapCoordinates) {
+
+}
+
+function setShipStart(coordinates) {
+
+}
+
+function moveShip(input, shipStart, map) {
+    
+}
+
